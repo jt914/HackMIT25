bdd063df43ed0262e05de328c4a31d70201bbddf
jt914 <josephtso914@gmail.com>
2025-09-13 19:44:55 -0400

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -40,7 +40,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:8000", "http://127.0.0.1:8000", "*"],  # tighten later
+    allow_origins=["*"],  # tighten later
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
